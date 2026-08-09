def2eddf9246ad5a3cc5543b28d1055e050aea57
Natanael Fernandes <filhonatanael01@gmail.com>
2026-08-09 18:52:59 -0300

    linked list

---
+++ neetcode/netcode_150/linked_list/reverse_linked_list.cs
@@ -0,0 +1,23 @@
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode list = new (head.val);
+        while(head.next != null)
+        {
+            head = head.next;
+            ListNode newItem = new(head.val, list);
+            list = newItem;
+        }
+        return list;
+    }
+}
