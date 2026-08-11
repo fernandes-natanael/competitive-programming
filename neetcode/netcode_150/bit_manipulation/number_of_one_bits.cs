0a1141dcd38146c3faae71b34cc64374e86654ec
Natanael Fernandes <filhonatanael01@gmail.com>
2026-08-11 20:42:06 -0300

    other solution

--- neetcode/netcode_150/bit_manipulation/number_of_one_bits.cs
+++ neetcode/netcode_150/bit_manipulation/number_of_one_bits.cs
@@ -1,10 +1,27 @@
+// better solution
 public class Solution {
     public int HammingWeight(uint n) {
         var countOne = 0;
-        while(n != 0) {
-            if((n & 1) == 1) countOne++;
-            n >>= 1;
+        for (int i = 0; i < 32; ++i) {
+            if (((1 << i) & n) != 0) 
+            // Check creat 1 10 100 ...
+            // Check each variation with n
+            // Then if the and action generates a
+            // number different of 0, so its a new 1
+                countOne++;
         }
         return countOne;
     }
 }
+
+// my solution
+// public class Solution {
+//     public int HammingWeight(uint n) {
+//         var countOne = 0;
+//         while(n != 0) {
+//             if((n & 1) == 1) countOne++;
+//             n >>= 1;
+//         }
+//         return countOne;
+//     }
+// }
